cd8daf167aa88830ffab93da50e944551007cca1
jessparnow@gmail.com <jessparnow@gmail.com>
2021-01-10 19:16:33 -0600

    trying yet again

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ let mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL){
-  connection = mysql,mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
